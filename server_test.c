48830a4f4c16c1365e1dd3dfd538efa19bf70f7b
Bharatvaj PH <bharatvaj@yahoo.com>
2017-09-06 12:47:28 +0530

    File Renamed

--- server_test.c
+++ server_test.c
@@ -5,5 +5,13 @@
 
 int main(int argc, char *argv[]){
 	int sockfd = start_server(DEFAULT_PORT);
+	while(1){
+		char *buf = (char *)malloc(1);
+		int bytes_read = 0;
+		if( (bytes_read = read(sockfd, buf, 1)) <= 0 ){
+			continue;
+		}
+		printf("%s", buf);
+	}
 	return 0;
 }
